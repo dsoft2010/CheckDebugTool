78c93de8965c04ad924e87b0dac5db4aad150bb7
Durk-jae Yun <durkjae.yun@directionsoft.com>
2023-03-10 19:37:09 +0900

    로그 추가

--- CheckDebug/src/main/cpp/check_debug_util.cpp
+++ CheckDebug/src/main/cpp/check_debug_util.cpp
@@ -1,6 +1,18 @@
 #include <jni.h>
 #include <string>
 #include <unistd.h>
+#include <android/log.h>
+
+#define  LOG_TAG    "CheckDebug"
+#define  LOGUNK(...)  __android_log_print(ANDROID_LOG_UNKNOWN,LOG_TAG,__VA_ARGS__)
+#define  LOGDEF(...)  __android_log_print(ANDROID_LOG_DEFAULT,LOG_TAG,__VA_ARGS__)
+#define  LOGV(...)  __android_log_print(ANDROID_LOG_VERBOSE,LOG_TAG,__VA_ARGS__)
+#define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
+#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
+#define  LOGW(...)  __android_log_print(ANDROID_LOG_WARN,LOG_TAG,__VA_ARGS__)
+#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+#define  LOGF(...)  __android_log_print(ANDROID_FATAL_ERROR,LOG_TAG,__VA_ARGS__)
+#define  LOGS(...)  __android_log_print(ANDROID_SILENT_ERROR,LOG_TAG,__VA_ARGS__)
 
 extern "C"
 JNIEXPORT jboolean JNICALL
@@ -8,6 +20,9 @@ Java_kr_ds_util_CheckDebugNativeLib_isDebugToolTracerPid(JNIEnv *env, jobject th
     int TPid;
     char buf[512];
     const char *str = "TracerPid:";
+
+    LOGD("isDebugToolTracerPid enter");
+
     size_t strSize = strlen(str);
     FILE *file = fopen("/proc/self/status", "r");
 
@@ -16,12 +31,14 @@ Java_kr_ds_util_CheckDebugNativeLib_isDebugToolTracerPid(JNIEnv *env, jobject th
             sscanf(buf, "TracerPid: %d", &TPid);
             if (TPid != 0) {
                 fclose(file);
+                LOGD("isDebugToolTracerPid %d", true);
                 return true;
             }
         }
     }
 
     fclose(file);
+    LOGD("isDebugToolTracerPid %d", false);
     return false;
 }
 extern "C"
@@ -30,19 +47,23 @@ Java_kr_ds_util_CheckDebugNativeLib_isDebugToolCmdLine(JNIEnv *env, jobject thiz
     char filePath[32], fileRead[128];
     FILE *file;
 
+    LOGD("isDebugToolCmdLine enter");
     snprintf(filePath, 24, "/proc/%d/cmdline", getppid());
     file = fopen(filePath, "r");
     if (file == nullptr) {
+        LOGD("isDebugToolCmdLine %d", false);
         return false;
     }
     fgets(fileRead, 128, file);
     fclose(file);
 
     if (!strcmp(fileRead, "gdb")) {
+        LOGD("isDebugToolCmdLine gdb %d", true);
         return true;
     }
 
     if (!strcmp(fileRead, "lldb")) {
+        LOGD("isDebugToolCmdLine lldb %d", true);
         return true;
     }
 
@@ -51,5 +72,6 @@ Java_kr_ds_util_CheckDebugNativeLib_isDebugToolCmdLine(JNIEnv *env, jobject thiz
 extern "C"
 JNIEXPORT jint JNICALL
 Java_kr_ds_util_CheckDebugNativeLib_version(JNIEnv *env, jobject thiz) {
+    LOGD("version %d", true);
     return 1;
 }
